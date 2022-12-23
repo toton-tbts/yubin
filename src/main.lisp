28a13117cfa358fda4b63f7229fc3cc400423d77
toton <toton.tbts@gmail.com>
2022-12-24 00:16:15 +0900

    Initial commit

---
+++ src/main.lisp
@@ -0,0 +1,20 @@
+(defpackage yubin
+  (:use :cl)
+  (:import-from #:jonathan #:parse)
+  (:export #:get-place))
+(in-package :yubin)
+
+(defun get-place (zipcode)
+  (let* ((url (quri:make-uri :defaults "http://zipcode.ibsnet.co.jp/api/search"
+                             :query '(("zipcode" . ,zipcode))))
+         (response (parse (dex:get url)))
+         (result (first (getf response :|results|))))
+    (if result
+        (concatenat 'string
+                    (getf result :|address1|)
+                    (getf result :|address2|)
+                    (getf result :|address3|))
+        (error (format nil "~A: ~S (Code=~A)"
+                       (getf response :|message|)
+                       zipcode
+                       (getf response :|status|))))))
